bab59871e3e57d98abc7b6a32ec9f3bae6ee65d1
Dyanko <dcisneros@grupoact.com>
2017-02-07 12:57:39 -0600

    Ahora el menú de Login está centrado verticalmente

--- application/views/login.php
+++ application/views/login.php
@@ -13,72 +13,70 @@
     <title>Grupo ACT</title>
     <!--Import jQuery before materialize.js-->
     <script src="<?= base_url()?>scripts/jquery-3.1.1.min.js"></script>
+    <style type="text/css">
+#padre {
+    padding: 5% 0;
+    /*background-color: blue;*/
+}
+#hijo {
+    padding: 1% 0;
+    /*background-color: yellow;*/
+    text-align: center;
+}
+    </style>
 </head>
 <body>
-    <!--Content-->
-    <br>
-    <br>
-    <div class="row">
-    <div class="col s1 m2 l4">
-    <!--/-->
-    </div>
-    <div class="col s10 m8 l4 z-depth-3 white center-align">
-        <!--Form-->
-        <br>
-        <img class="responsive-img" src="<?= base_url()?>image/logo_act.png" width="300">
-        <br>
-        <blockquote><h5 class="grey lighten-3" style="color: #808080">Control de Visitas</h5></blockquote>
-        <br>
-        <!--User-->
-        <?= form_open('Login/validation'); ?>
+<div id="padre">
+    <div id="hijo">
+        
         <div class="row">
-            <div class="col l3 m3 s12">
-                <i class="material-icons prefix">account_circle</i>
-            </div>
-            <div class="col l6 m6 s12">
-                <input type="text" class="validate" style="text-align: center" name="Usuario"  placeholder="Usuario">
-            </div>
-            <div class="col l3 m3 s12">
-            </div>
+        <div class="col s1 m3 l4">
         </div>
-        <!--Password-->
-        <div class="row">
-            <div class="col l3 m13 s12">
-                <i class="material-icons prefix">lock</i>
-            </div>
-            <div class="col l6 m6 s12">
-                <input type="password" class="validate" style="text-align: center" name="Password"  placeholder="Password">
-            </div>
-            <div class="col l3 m3 s12">
-            </div>
-        </div>
-        <!--Button-->
-        <div class="row" style="text-align: center;">
+        <div class="col s10 m6 l4 z-depth-3 white">
+            <!--Header-->
+            <div style="padding: 25px">
+            <img class="responsive-img" src="<?= base_url()?>image/logo_act.png" width="250">
+            <blockquote><h5 class="grey lighten-3" style="color: #808080">Control de Visitas</h5></blockquote>
+            <!--Inputs-->
+            <?= form_open('Login/validation'); ?>
+            <i class="material-icons prefix">account_circle</i>
+            <input type="text" class="validate" style="text-align: center" name="Usuario"  placeholder="Usuario">
+            <i class="material-icons prefix">lock</i>
+            <input type="password" class="validate" style="text-align: center" name="Password"  placeholder="Password">
+            <!--Button-->
             <button type="submit" name="submit" class="waves-effect waves-light blue btn"><i class="material-icons left">lock_open</i>
                 Ingresar
             </button>
+            </div>
+        </div>
+        <div class="col s1 m3 l4">
+        </div>
         </div>
         <!--Login Feedback-->
-        <?php
-            if ($title == "")
-            {
-                echo "";
-            }
-            else
-            {
-                echo "<div class='red white-text'> $title </div><br>";            
-            }
-        ?>
-        <?php
-            echo form_close();
-            echo validation_errors('<div class="red white-text">', '</div><br>');
-        ?>
+        <div class="row">
+        <div class="col s1 m3 l4">
+        </div>
+        <div class="col s10 m6 l4">
+            <?php
+                if ($title == "")
+                {
+                    echo "";
+                }
+                else
+                {
+                    echo "<div class='red white-text'> $title </div><br>";
+                }
+            ?>
+            <?php
+                echo form_close();
+                echo validation_errors('<div class="red white-text">', '</div><br>');
+            ?>
+        </div>
+        <div class="col s1 m3 l4">
+        </div>
+        </div>
+
     </div>
-        <!--/Form-->
-    </div>
-    <div class="col s1 m2 l4">
-    <!--/-->
-    </div>
-    </div>
+</div>
 </body>
 </html>
