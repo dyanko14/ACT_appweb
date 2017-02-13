d01437482cdea4eba76fed3fe7e234f21338cf11
Dyanko <dcisneros@grupoact.com>
2017-02-13 16:21:50 -0600

    La IP del base URL del server ahora se toma de forma automática

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.0.57/';
+$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'];
 /*
 |--------------------------------------------------------------------------
 | Index File
