0433d86e5a7ef5fdbf4d012c61176a27249c65f8
Dyanko <dcisneros@grupoact.com>
2017-02-28 14:03:12 -0600

    Correción en módulo de hora de salida

--- application/views/user/registros_edit.php
+++ application/views/user/registros_edit.php
@@ -1,5 +1,18 @@
+<!--Header-->
+<div class="row" style="border-bottom: solid 1px red">
+  <div class="col l2 m2 s2 center-align">
+    <br>
+    <ul>
+      <li><a href="#" data-activates="slide-out" class="btn-floating red button-collapse" class="btn-floating blue"><i class="material-icons">menu</i></a></li>
+    </ul>
+  </div>
+  <div class="col l10 m10 s10 center-align">            
+    <br>
+    <h5><?= $title ?></h5>
+  </div>
+</div>
+<!--/Header-->
 <main>
-<br>
 <div class="row">
 <div class="col s1 m2 l2">
 <!---->
@@ -77,3 +90,8 @@ foreach ($query->result() as $row){
     <div class="col s1 m2 l2">
     </div>
 </div>
+</tbody>
+</div>
+</main>
+
+</body>
